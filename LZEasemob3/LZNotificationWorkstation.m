3d5d6e8aac80737fe68a420be6ca49ea524703cd
zhen.lei <zhen.lei@kindroid.com>
2016-12-06 01:16:59 +0800

    去掉多余代码

--- LZEasemob3/LZNotificationWorkstation.m
+++ LZEasemob3/LZNotificationWorkstation.m
@@ -29,28 +29,6 @@
 //
 
 
-//NSString *const KSwitchRootViewControllerNotification = @"KSwitchRootViewControllerNotification";
-//
-//NSString *const KGoToLoginViewControllerNotification = @"KGoToLoginViewControllerNotification";
-//
-//NSString *const KGotoChangePasswordViewControllerNotification = @"KGotoChangePasswordViewControllerNotification";
-//
-//NSString *const KGotoWriteCompanyInfoViewControllerNotification = @"KGotoWriteCompanyInfoViewControllerNotification";
-//
-//#pragma mark - LZEditCollectionViewController
-//NSString *const KEditCollectionViewControllerBackNotification = @"KEditCollectionViewControllerBackNotification";
-//
-//
-//#pragma mark - LZMallsGoodsView
-//NSString *const KMallsGoodsViewControllerIndividualCommercialNotification = @"KMallsGoodsViewControllerIndividualCommercialNotification";
-//NSString *const KMallsGoodsViewControllerIndividualCommercialKey = @"KMallsGoodsViewControllerIndividualCommercialKey";
-//NSString *const KMallsGoodsViewControllerIndividualIndexPathKey = @"KMallsGoodsViewControllerIndividualIndexPathKey";
-//
-//#pragma mark - LZBillingDetailsContainerView
-//NSString *const KBillingDetailsContainerViewDidSelectBtnTypeNotification = @"KBillingDetailsContainerViewDidSelectBtnTypeNotification";
-//NSString *const KBillingDetailsContainerViewDidSelectBtnKey = @"KBillingDetailsContainerViewDidSelectBtnKey";
-
-
 #pragma mark -Contacts
 NSString *const KRefurbishContactsViewControllerNotification = @"KRefurbishContactsViewControllerNotification";
 
